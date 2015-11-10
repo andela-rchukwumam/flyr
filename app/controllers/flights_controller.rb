5daa45373f294a25ae905b557c76aa5f38969a6b
andela-rchukwumam <aruth71@gmail.com>
2015-11-10 17:19:10 +0100

    Almost done with the login feature

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,24 +1,24 @@
 class FlightsController < ApplicationController
 	def index
-	    # @flight = Flight.new
+	    @flight = Flight.new
 
 	    @airports = Airport.all.map { |airport| [airport.city, airport.name] }
 	    @dates = Flight.order("flight_date asc").all.map { |flight| [flight.flight_date.strftime("%d/%m/%Y")] }.uniq
 	    @passengers = [1, 2, 3, 4]
 
 
-	    # if !params[:flight].nil?
-	    #   @from = params[:flight][:from_airport]
-	    #   @to = params[:flight][:to_airport]
-	    #   @date = params[:flight][:flight_date]
-	    #   @passengers_select = params[:flight][:passengers]
-	    #   @flights = Flight.search(@from, @to, @date)
+	    if !params[:flight].nil?
+	      @from = params[:flight][:from_airport]
+	      @to = params[:flight][:to_airport]
+	      @date = params[:flight][:flight_date]
+	      @passengers_select = params[:flight][:passengers]
+	      @flights = Flight.search(@from, @to, @date)
 
-	    #   respond_to do |format|
-	    #     format.html
-	    #     format.js
-	    #   end
-	    # end
+	      respond_to do |format|
+	        format.html
+	        format.js
+	      end
+	    end
 	end
 
 	def about
