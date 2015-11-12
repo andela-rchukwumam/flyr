d53c0ee4047d0e6cbbc94fa2579475f773410c4a
andela-rchukwumam <aruth71@gmail.com>
2015-11-12 01:01:55 +0100

    FInished Feature Search

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ActiveRecord::Base
-	belongs_to :departure, class_name: Airport
-  	belongs_to :arrival, class_name: Airport
+	belongs_to :departure, class_name: Airport, foreign_key: :dept_id
+  	belongs_to :arrival, class_name: Airport, foreign_key: :arr_id
 
   	has_many :bookings, class_name: Booking, foreign_key: :booking_id
   	has_many :passengers, through: :bookings
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   get 'logout'  => 'sessions#destroy'
+  get 'search' => 'flights#search', as: "flights_search"
   resources :users
 end
